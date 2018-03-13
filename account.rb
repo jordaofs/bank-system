0d18d310f4cccb115f1c99fbf17f462af26bd5df
Jordão Frazão <jordaofrazao@gmail.com>
2018-03-13 16:38:09 -0300

    Update account.rb

--- account.rb
+++ account.rb
@@ -1,8 +1,8 @@
 class Account
-	attr_accessor :cpf, :number, :balance
-	def initialize(cpf, number, balance)
-		@cpf = cpf
-		@number = number
-		@balance = balance
-	end
+  attr_accessor :cpf, :number, :balance
+    def initialize(cpf, number, balance)
+      @cpf = cpf
+      @number = number
+      @balance = balance
+    end
 end
